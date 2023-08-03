81f5a31b98412751ae69029e8a8c5cd7f0804db5
Kalyan Kumar Choubey <kalyanchoubey98@gmail.com>
2023-08-03 23:19:56 +0530

    Dynamic Init

--- main.py
+++ main.py
@@ -19,17 +19,32 @@ def prepare_data(df,forecast_col,forecast_out,test_size):
     response = [X_train,X_test , Y_train, Y_test , X_lately]
     return response
 
-df = pd.read_csv("prices.csv")
-df = df[df.symbol == "MAZDOCK"]
+# Taking Input for Filename
+fileName = input("Enter the file name to analyze (csv): ")
+
+# Reading the file
+df = pd.read_csv(fileName)
+
+# Reading the symbol
+stockName = input("Enter the stock to analyze: ")
+
+# Filtering the Stock Prices
+df = df[df.symbol == stockName]
+
+# Choosing the open or close file
+priceOption = input("Choose from the following:\n1. Open\n2. Close\n3. High\n4. Low\n\nYour Selection: ")
+
+def processPriceOption(option):
+
+    return 'close'
 
 # Preparing Controller Variables
-
 forecast_col = 'close'
 forecast_out = 5
 test_size = 0.2
 
 # Initializing linear regression model
-X_train, X_test, Y_train, Y_test , X_lately =prepare_data(df,forecast_col,forecast_out,test_size); #calling the method were the cross validation and data preperation is in
+X_train, X_test, Y_train, Y_test , X_lately = prepare_data(df,forecast_col,forecast_out,test_size); #calling the method were the cross validation and data preperation is in
 learner = LinearRegression() 
 
 # Training the linear regression model
